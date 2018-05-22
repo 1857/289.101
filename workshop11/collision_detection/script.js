a702b060054579434f1178b6a01b0d07e3d3c280
Kearns-Hill <17051307@wsom-072799.massey.ac.nz>
2018-05-22 15:14:32 +1200

    up down loged

--- workshop11/collision_detection/script.js
+++ workshop11/collision_detection/script.js
@@ -9,3 +9,44 @@ Next, add the following features:
 - create an item that makes the puck smaller
 
 */
+
+var puck_x = 210;
+var puck_y = 210;
+
+var puck = document.getElementById('puck');
+var speed = 50
+
+function logCoordinates(){
+    console.log('x='+ puck_x + 'y='+ puck_y);
+}
+
+puck.style.left = puck_x + 'px';
+puck.style.top = puck_y + 'px';
+
+//move left
+document.getElementById('left').addEventListener('click', function(){
+   puck_x -= speed;
+   puck.style.left = puck_x + 'px';
+   logCoordinates();
+});
+
+//move right
+document.getElementById('right').addEventListener('click', function(){
+   puck_x += speed;
+   puck.style.left = puck_x + 'px';
+   logCoordinates();
+});
+
+//move up
+document.getElementById('up').addEventListener('click', function(){
+   puck_y -= speed;
+   puck.style.top = puck_y + 'px';
+   logCoordinates();
+});
+
+//move down
+document.getElementById('down').addEventListener('click', function(){
+   puck_y += speed;
+   puck.style.top = puck_y + 'px';
+   logCoordinates();
+});
